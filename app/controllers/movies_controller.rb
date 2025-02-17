0090c1b393c9e776ea3eb9b83de48a9162327c12
Jakub <000jcz@gmail.com>
2025-02-17 13:10:26 +0100

    Rubocop changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,17 +1,17 @@
 class MoviesController < ApplicationController
     before_action :set_movie, only: %i[ show edit update destroy ]
-   
+
     def index
       @movies = Movie.all
     end
-   
+
     def show
     end
-   
+
     def new
       @movie = Movie.new
     end
-   
+
     def create
       @movie = Movie.new(movie_params)
       if @movie.save
@@ -20,29 +20,29 @@ class MoviesController < ApplicationController
         render :new, status: :unprocessable_entity
       end
     end
-   
+
     def edit
     end
-   
+
     def update
       if @movie.update(movie_params)
-        redirect_to @movie 
+        redirect_to @movie
       else
         render :edit, status: :unprocessable_entity
       end
     end
-   
+
     def destroy
       @movie.destroy
       redirect_to movies_path
     end
-   
+
     private
       def set_movie
         @movie = Movie.find(params[:id])
       end
-   
+
       def movie_params
         params.require(:movie).permit(:title, :description, :duration_minutes, :origin_country, :genre_id, :director_id)
       end
-   end
+end
