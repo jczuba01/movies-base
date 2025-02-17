5ec6b0c3b585cdeb619b4a9894031fdf43f98564
Jakub <000jcz@gmail.com>
2025-02-17 16:28:11 +0100

    Test delete

--- db/migrate/20250217143313_remove_default_from_genres_user_id.rb
+++ db/migrate/20250217143313_remove_default_from_genres_user_id.rb
@@ -1,4 +1,4 @@
-class RemoveDefaultFromGenresUserId < ActiveRecord::Migration[6.0]
+class RemoveDefaultFromGenresUserId < ActiveRecord::Migration[8.0]
   def change
     change_column_default :genres, :user_id, nil
   end
