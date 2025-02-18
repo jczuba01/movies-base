d1f1ff9b15fb9c871675598b0985385848bb8cc4
Jakub <000jcz@gmail.com>
2025-02-18 12:28:57 +0100

    generate model Review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_18_100511) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_18_112823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -41,6 +41,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_18_100511) do
     t.index ["genre_id"], name: "index_movies_on_genre_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.bigint "user_id", null: false
+    t.bigint "movie_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -55,4 +66,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_18_100511) do
 
   add_foreign_key "movies", "directors"
   add_foreign_key "movies", "genres"
+  add_foreign_key "reviews", "movies"
+  add_foreign_key "reviews", "users"
 end
