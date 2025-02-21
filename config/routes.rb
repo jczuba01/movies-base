6a06858ada5b254544c28f798aa74e486f8086f9
Jakub <000jcz@gmail.com>
2025-02-22 00:32:30 +0100

    Update show update destroy methods

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     # API routes
     namespace :api do
       namespace :v1 do
-        resources :movies, only: [:index]
+        resources :movies
       end
     end
 
