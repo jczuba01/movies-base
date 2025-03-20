cd739f95527035f5ee3237652d5c1e50170a5438
Jakub <000jcz@gmail.com>
2025-03-20 15:56:16 +0100

    Added API sorting and filtering for movies & updated test

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,82 +1,94 @@
 class Api::V1::MoviesController < ActionController::Base
-    skip_before_action :verify_authenticity_token
-    before_action :set_movie, only: %i[ show update destroy ]
+  skip_before_action :verify_authenticity_token
+  before_action :set_movie, only: %i[ show update destroy ]
 
-    def index
-        if params[:director_id]
-          @director = Director.find_by(id: params[:director_id])
+  def index
+      if params[:director_id]
+        @director = Director.find_by(id: params[:director_id])
 
-          if @director
-            @movies = @director.movies
-            render json: @movies.as_json(include: {
-                genre: { only: [ :id, :name ] },
-                director: { only: [ :id, :first_name, :last_name ] }
-            }), status: :ok
-          else
-            render json: { error: "Director not found" }, status: :not_found
-          end
-        elsif params[:genre_id]
-          @genre = Genre.find_by(id: params[:genre_id])
-
-          if @genre
-            @movies = @genre.movies
-            render json: @movies.as_json(include: {
-                genre: { only: [ :id, :name ] },
-                director: { only: [ :id, :first_name, :last_name ] }
-            }), status: :ok
-          else
-            render json: { error: "Genre not found" }, status: :not_found
-          end
-        else
-          @movies = Movie.all
+        if @director
+          @movies = Api::MoviesFilterService.new(@director.movies, filter_params).call
           render json: @movies.as_json(include: {
-            genre: { only: [ :id, :name ] },
-            director: { only: [ :id, :first_name, :last_name ] }
+              genre: { only: [ :id, :name ] },
+              director: { only: [ :id, :first_name, :last_name ] }
           }), status: :ok
-        end
-    end
-
-    def show
-        render json: MovieSerializer.new(@movie).serialize, status: :ok
-    end
-
-    def create
-        @movie = Movie.new(movie_params)
-
-        if @movie.save
-            render json: @movie, status: :created
         else
-            render json: { errors: @movie.errors.full_messages }, status: :unprocessable_entity
+          render json: { error: "Director not found" }, status: :not_found
         end
-    end
+      elsif params[:genre_id]
+        @genre = Genre.find_by(id: params[:genre_id])
 
-    def update
-        if @movie.update(movie_params)
-            render json: @movie, status: :ok
+        if @genre
+          @movies = @genre.movies
+          render json: @movies.as_json(include: {
+              genre: { only: [ :id, :name ] },
+              director: { only: [ :id, :first_name, :last_name ] }
+          }), status: :ok
         else
-            render json: { errors: @movie.errors.full_messages }, status: :unprocessable_entity
+          render json: { error: "Genre not found" }, status: :not_found
         end
-    end
+      else
+        @movies = Api::MoviesFilterService.new(Movie.all, filter_params).call
+        render json: @movies.as_json(include: {
+          genre: { only: [ :id, :name ] },
+          director: { only: [ :id, :first_name, :last_name ] }
+        }), status: :ok
+      end
+  end
 
-    def destroy
-        @movie.destroy
-        head :no_content
-    end
+  def show
+      render json: MovieSerializer.new(@movie).serialize, status: :ok
+  end
 
-    private
+  def create
+      @movie = Movie.new(movie_params)
 
-    def set_movie
-        @movie = Movie.find(params[:id])
-    end
+      if @movie.save
+          render json: @movie, status: :created
+      else
+          render json: { errors: @movie.errors.full_messages }, status: :unprocessable_entity
+      end
+  end
 
-    def movie_params
-        params.require(:movie).permit(
-            :title,
-            :description,
-            :duration_minutes,
-            :origin_country,
-            :genre_id,
-            :director_id
-        )
-    end
+  def update
+      if @movie.update(movie_params)
+          render json: @movie, status: :ok
+      else
+          render json: { errors: @movie.errors.full_messages }, status: :unprocessable_entity
+      end
+  end
+
+  def destroy
+      @movie.destroy
+      head :no_content
+  end
+
+  private
+
+  def set_movie
+      @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+      params.require(:movie).permit(
+          :title,
+          :description,
+          :duration_minutes,
+          :origin_country,
+          :genre_id,
+          :director_id
+      )
+  end
+
+  def filter_params
+      params.permit(
+          :title, 
+          :max_duration, 
+          :origin_country, 
+          :sort, 
+          :direction,
+          :genre_name,
+          :director_last_name
+      )
+  end
 end
---
+++ app/services/api/movies_filter_service.rb
@@ -0,0 +1,41 @@
+module Api
+  class MoviesFilterService < Api::FilterService
+    private
+
+    def apply_filters(result)
+      result = filter_by_title(result)
+      result = filter_by_genre_name(result)
+      result = filter_by_director_last_name(result) 
+      result = filter_by_duration(result)
+      result = filter_by_origin_country(result)
+      result
+    end
+
+    def filter_by_title(result)
+      return result unless params[:title].present?
+      result.where("title ILIKE ?", "%#{params[:title]}%")
+    end
+
+    def filter_by_genre_name(result)
+      return result unless params[:genre_name].present?
+      result = result.joins(:genre)
+      result.where("genres.name ILIKE ?", "%#{params[:genre_name]}%")
+    end
+
+    def filter_by_director_last_name(result)
+      return result unless params[:director_last_name].present?
+      result = result.joins(:director)
+      result.where("directors.last_name ILIKE ?", "%#{params[:director_last_name]}%")
+    end
+
+    def filter_by_duration(result)
+      return result unless params[:max_duration].present?
+      result.where("duration_minutes <= ?", params[:max_duration])
+    end
+
+    def filter_by_origin_country(result)
+      return result unless params[:origin_country].present?
+      result.where(origin_country: params[:origin_country])
+    end
+  end
+end
